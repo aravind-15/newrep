b97de90306ca1ed5262d93032682c281e77c0c61
ARAVIND <aravind-15>
2020-07-23 16:04:04 +0530

    yess

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 var express = require('express');
 var app = express();
-app.get('/',(req,res) =>
-{res.send("home");});
+app.get('/',function(req,res) {
+    res.send('home page');
+});
 
 var port=process.env.PORT ||3000;
-app.listen( port,function()
-{
-
-
+app.listen( port,function(){
 console.log('web server is listening at port' + port);
 }); 
