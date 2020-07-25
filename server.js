6689b4812429bd80e5f85de103f9281775e9db66
ARAVIND <aravind-15>
2020-07-25 10:54:15 +0530

    added msg

---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+app.get('/',function(req,res) {
+    res.sendFile(__dirname+"/frontend/html/index.html");
+});
+app.get('/texttags',function(req,res) {
+    res.sendFile(__dirname+"/frontend/html/texttags.html");
+});
+
+var port=process.env.PORT ||4000;
+app.listen( port,function(){
+console.log('web server is listening at port' + port);
+});  
